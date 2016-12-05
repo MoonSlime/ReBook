3a0122467fc684715315de8ea4ee047af098064a
MoonSlime <sck3578@naver.com>
2016-12-05 17:10:16 +0900

    아싸 성공~~~~

--- app/src/main/java/com/cksrb/rebook/ChatActivity.java
+++ app/src/main/java/com/cksrb/rebook/ChatActivity.java
@@ -5,10 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.cksrb.rebook.DataForm.ChatData;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -45,16 +49,16 @@ public class ChatActivity extends AppCompatActivity {
 
         //
 
-        prepareData();
+  //      prepareData();
         //
-/*
+
         editText = (EditText)findViewById(R.id.editText);
         button_Push = (Button)findViewById(R.id.button_Push);
 
         button_Push.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ChatData chatData = new ChatData("userName", editText.getText().toString());  // 유저 이름과 메세지로 chatData 만들기
+                ChatData chatData = new ChatData("User_Id", editText.getText().toString());  // 유저 이름과 메세지로 chatData 만들기
                 databaseReference.child("message").push().setValue(chatData);  // 기본 database 하위 message라는 child에 chatData를 list로 만들기
                 editText.setText("");
             }
@@ -64,8 +68,10 @@ public class ChatActivity extends AppCompatActivity {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                 ChatData chatData = dataSnapshot.getValue(ChatData.class);  // chatData를 가져오고
-                TextView textView = (TextView)findViewById(R.id.textView);
-                textView.setText(chatData.getUserId()+"send this\n=>"+chatData.getMessage());  // adapter에 추가합니다.
+                chatDataList.add(chatData);
+                cAdapter.notifyDataSetChanged();
+                //TextView textView = (TextView)findViewById(R.id.textView);
+                //textView.setText(chatData.getUserId()+"send this\n=>"+chatData.getMessage());  // adapter에 추가합니다.
             }
 
             @Override
@@ -80,7 +86,7 @@ public class ChatActivity extends AppCompatActivity {
             @Override
             public void onCancelled(DatabaseError databaseError) { }
         });
-        */
+
     }
 
     private void prepareData(){
