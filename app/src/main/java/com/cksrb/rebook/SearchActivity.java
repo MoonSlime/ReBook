3e7b45c4265c0d121ff1064648d538ad8cacb8f9
MoonSlime <sck3578@naver.com>
2016-12-05 22:45:45 +0900

    블로그 검색 가능??

--- app/src/main/java/com/cksrb/rebook/SearchActivity.java
+++ app/src/main/java/com/cksrb/rebook/SearchActivity.java
@@ -1,7 +1,9 @@
 package com.cksrb.rebook;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -13,6 +15,7 @@ import java.net.URL;
 import java.net.URLEncoder;
 
 public class SearchActivity extends AppCompatActivity {
+    private MyAsyncTask myAsyncTask;
 
     EditText editText;
     Button button;
@@ -27,34 +30,75 @@ public class SearchActivity extends AppCompatActivity {
         button = (Button)findViewById(R.id.button_Search);
         textView = (TextView)findViewById(R.id.textView_Search);
 
+        myAsyncTask = new MyAsyncTask();
+        myAsyncTask.execute("80", "90", "100", "110");
+    }
 
-        String clientId = "H6Vntz4GAyzdZlEZLgHq";
-        String clientSecret = "3ghV0ENhbJ";
-        try {
-            String text = URLEncoder.encode("그린팩토리", "UTF-8");
-            String apiURL = "https://openapi.naver.com/v1/search/blog.xml?query="+ text;
-            URL url = new URL(apiURL);
-            HttpURLConnection con = (HttpURLConnection)url.openConnection();
-            con.setRequestMethod("GET");
-            con.setRequestProperty("X-Naver-Client-Id", clientId);
-            con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
-            int responseCode = con.getResponseCode();
-            BufferedReader br;
-            if(responseCode==200) { // 정상 호출
-                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
-            } else {  // 에러 발생
-                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+    public class MyAsyncTask extends AsyncTask<String,Void,String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String...params) {
+            String str = "Hello";
+
+            String clientId = "H6Vntz4GAyzdZlEZLgHq";//애플리케이션 클라이언트 아이디값";
+            String clientSecret = "3ghV0ENhbJ";//애플리케이션 클라이언트 시크릿값";
+            try {
+                String text = URLEncoder.encode("그린팩토리", "UTF-8");
+                String apiURL = "https://openapi.naver.com/v1/search/blog?query="+ text; // json 결과
+                //String apiURL = "https://openapi.naver.com/v1/search/blog.xml?query="+ text; // xml 결과
+                URL url = new URL(apiURL);
+                HttpURLConnection con = (HttpURLConnection)url.openConnection();
+                con.setRequestMethod("GET");
+                con.setRequestProperty("X-Naver-Client-Id", clientId);
+                con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
+                int responseCode = con.getResponseCode();
+                BufferedReader br;
+                if(responseCode==200) { // 정상 호출
+                    br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                } else {  // 에러 발생
+                    br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+                }
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+                while ((inputLine = br.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                br.close();
+                str=response.toString();
+            } catch (Exception e) {
+                str=""+e;
             }
-            String inputLine;
-            StringBuffer response = new StringBuffer();
-            while ((inputLine = br.readLine()) != null) {
-                response.append(inputLine);
+
+
+
+
+
+
+            return str;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Log.d("debug",result);
+            textView.setText(result);
+            //super.onPostExecute(result);
+
+            if(result != null){
+                Log.d("ASYNC", "result = " + result);
             }
-            br.close();
-            textView.setText(response.toString());
-        } catch (Exception e) {
-            System.out.println(e);
+
+        }
+
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
         }
 
     }
+
 }
