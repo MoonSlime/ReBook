406b640b4bb9fa87491941a8698e735e9c77baeb
Dugg\Duck <leeduck13@naver.com>
2016-12-08 20:37:36 +0900

    ui_16.12.08_ver1

--- app/src/main/java/com/cksrb/rebook/MainActivity.java
+++ app/src/main/java/com/cksrb/rebook/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -47,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
 
+/*
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-
+*/
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
--- app/src/main/java/com/cksrb/rebook/WishFragment.java
+++ app/src/main/java/com/cksrb/rebook/WishFragment.java
@@ -3,16 +3,39 @@ package com.cksrb.rebook;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
-public class WishFragment extends Fragment {
+public class WishFragment extends Fragment{
+        private ListView booklist;
+        private ListViewAdapterWish adapter;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_wish, container, false);
+        View view = inflater.inflate(R.layout.fragment_wish, null);
+
+        adapter = new ListViewAdapterWish(getContext());
+        booklist = (ListView) view.findViewById(R.id.wishList);
+        booklist.setAdapter(adapter);
+
+        for(int i = 1; i < 10; i++) {
+            addData(); // add Data
+        }
+
+        adapter.notifyDataSetChanged();
+
+        return view;
+    }
+
+    public void addData(){
+        ListViewItem u1 = new ListViewItem(getResources().getDrawable(R.drawable.ic_menu_send),
+                "나는 장바구니", "살거냣");
+        adapter.addData(u1); // add list data
     }
 }
