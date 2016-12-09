82d177a009cb9aaaa6367b9984f79b3c910abb9b
MoonSlime <sck3578@naver.com>
2016-12-09 18:19:23 +0900

    ㅇ

--- app/src/main/java/com/cksrb/rebook/Fragment/DealFragment.java
+++ app/src/main/java/com/cksrb/rebook/Fragment/DealFragment.java
@@ -7,27 +7,33 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
+import com.cksrb.rebook.DataForm.BookData;
 import com.cksrb.rebook.ListViewAdapter.ListViewAdapterDeal;
 import com.cksrb.rebook.ListViewItem;
 import com.cksrb.rebook.R;
+import com.cksrb.rebook.ReBookApplication;
 
-    public class DealFragment extends Fragment{
-        private ListView booklist;
-        private ListViewAdapterDeal adapter;
+import java.util.List;
+
+public class DealFragment extends Fragment{
+
+    private ReBookApplication app;
+    private ListView booklist;
+    private ListViewAdapterDeal adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_deal, null);
 
+        app = (ReBookApplication)getContext().getApplicationContext();
+
         adapter = new ListViewAdapterDeal(getContext());
         booklist = (ListView) view.findViewById(R.id.dealList);
         booklist.setItemsCanFocus(false);
         booklist.setAdapter(adapter);
 
-        //for(int i = 1; i <= 10; i++) {
-            addData();
-        //}
+        addData();
 
         adapter.notifyDataSetChanged();
 
@@ -35,6 +41,24 @@ import com.cksrb.rebook.R;
     }
 
     public void addData(){
+        List<BookData> bookDataList = app.getBookList();
+
+        int i=bookDataList.size();
+        for(;i>0;--i){
+            if(bookDataList.get(i-1).getSellerId().equals(app.getUserId())){
+                ListViewItem u=null;
+                if(bookDataList.get(i-1).getCustomerId()!=null){
+                    u = new ListViewItem(getResources().getDrawable(R.drawable.ic_menu_manage),
+                            bookDataList.get(i-1).getTitle(),"구매자 : "+bookDataList.get(i-1).getCustomerId());
+                }
+                else{
+                    u = new ListViewItem(getResources().getDrawable(R.drawable.ic_menu_manage),
+                            bookDataList.get(i-1).getTitle(),"구매자 : 없음");
+                }
+                adapter.addData(u);
+            }
+        }
+/*
         ListViewItem u1 = new ListViewItem(getResources().getDrawable(R.drawable.ic_menu_manage),
                 "거래현황 1", "채팅해랏");
         adapter.addData(u1); // add list data
@@ -42,5 +66,6 @@ import com.cksrb.rebook.R;
         u1 = new ListViewItem(getResources().getDrawable(R.drawable.ic_menu_manage),
                 "거래현황 2", "채팅해랏");
         adapter.addData(u1); // add list data
+*/
     }
 }
